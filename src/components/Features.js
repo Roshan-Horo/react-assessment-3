fcfb2368fe60181cb7bb39e8e2d3115736c90412
Roshan-Horo <35597955+Roshan-Horo@users.noreply.github.com>
2021-03-18 14:05:43 +0530

    includes features

--- src/components/Features.js
+++ src/components/Features.js
@@ -5,7 +5,42 @@ const Features = () => {
         <section className="features" id="features">
               <h1 className="heading">Features</h1>
               <div className="triangle-box"></div>
-              <div className="features-info"></div>
+              <div className="features-info">
+               <div className="info">
+                <p>Lorem ipsum dolor, sit amet 
+                consectetur adipisicing elit. 
+                Necessitatibus ad tempora explicabo
+                 perspiciatis numquam quos fuga iure, 
+                 fugiat dicta. Incidunt, vitae! Eum 
+                 illum porro eos sequi suscipit voluptas 
+                 recusandae in earum quibusdam a? Cum 
+                 vero officia eaque sit ducimus nisi! 
+                 Temporibus quam sapiente inventore modi, 
+                 nisi ipsam vel perferendis voluptas.
+                 
+                 Lorem ipsum dolor sit amet consectetur 
+                adipisicing elit. Ducimus quaerat inventore 
+                architecto nihil pariatur fugit 
+                ullam tempora soluta rem facere.
+                
+                Lorem ipsum dolor, sit amet 
+                consectetur adipisicing elit. 
+                Necessitatibus ad tempora explicabo
+                 perspiciatis numquam quos fuga iure, 
+                 fugiat dicta. Incidunt, vitae! Eum 
+                 illum porro eos sequi suscipit voluptas 
+                 recusandae in earum quibusdam a? Cum 
+                 vero officia eaque sit ducimus nisi! 
+                 Temporibus quam sapiente inventore modi, 
+                 nisi ipsam vel perferendis voluptas.</p>
+               </div>
+               <div className="feature">
+                <p>Lorem ipsum dolor sit amet consectetur 
+                adipisicing elit. Ducimus quaerat inventore 
+                architecto nihil pariatur fugit 
+                ullam tempora soluta rem facere.</p>
+               </div>
+              </div>
           </section>
     )
 }
